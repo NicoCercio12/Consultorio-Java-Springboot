38c9f14b357883473797036c056d25ed8b8e81f2
NicoCercio12 <cercio1998@gmail.com>
2026-02-12 23:23:07 -0300

    Implementaciones en medico

--- src/main/java/com/consultorio/consultorio/repository/MedicoRepository.java
+++ src/main/java/com/consultorio/consultorio/repository/MedicoRepository.java
@@ -3,9 +3,13 @@ package com.consultorio.consultorio.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.consultorio.consultorio.model.Medico;
 import java.util.Optional;
+import java.util.List;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
-    Optional<Medico> findfindByMatricula(String matricula); //Busca por matricula
+    Optional<Medico> findByMatricula(String matricula); //Busca por matricula
+    Optional<Medico> findByDni(Long dni); //Busca por dni
+    List<Medico> findByEspecialidad(String especialidad); //Busca por especialida
+    
 
 }
